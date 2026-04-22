cc78354a11876e99f6cfec4b25a54bf803c9d1c2
rpatterson-fubotv <rpatterson@fubo.tv>
2026-04-22 17:21:33 -0600

    Add assessment component with MCQ + diagnostic flow

--- context-and-sessions/src/register-components.js
+++ context-and-sessions/src/register-components.js
@@ -2,12 +2,14 @@
 // is built, import it here and add it to registerComponent.
 import { registerComponent } from './component-registry.js'
 import AttentionBudgetAllocator from './components/AttentionBudgetAllocator.jsx'
+import ContextSessionsAssessment from './components/ContextSessionsAssessment.jsx'
 import ContextWindowPlayground from './components/ContextWindowPlayground.jsx'
 import LeverComparisonTool from './components/LeverComparisonTool.jsx'
 import LostInTheMiddleCurve from './components/LostInTheMiddleCurve.jsx'
 import SessionHygieneSimulator from './components/SessionHygieneSimulator.jsx'
 
 registerComponent('AttentionBudgetAllocator', AttentionBudgetAllocator)
+registerComponent('ContextSessionsAssessment', ContextSessionsAssessment)
 registerComponent('ContextWindowPlayground', ContextWindowPlayground)
 registerComponent('LeverComparisonTool', LeverComparisonTool)
 registerComponent('LostInTheMiddleCurve', LostInTheMiddleCurve)
